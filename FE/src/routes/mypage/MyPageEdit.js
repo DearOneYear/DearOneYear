48fa9572fab4a15bda04a8466ffb6d53ed6c4dd2
Suji Cha <suji0509@naver.com>
2022-11-23 14:39:41 +0900

    fix: 생일만 수정 가능

--- FE/src/routes/mypage/MyPageEdit.js
+++ FE/src/routes/mypage/MyPageEdit.js
@@ -7,8 +7,6 @@ const MyPageEdit = () => {
   const currBirthMonth = 5;
   const currBirthDate = 9;
 
-  const [name, setName] = useState(currName);
-  const [email, setEmail] = useState(currEmail);
   const [birthMonth, setBirthMonth] = useState(currBirthMonth);
   const [birthDate, setBirthDate] = useState(currBirthDate);
   const [maxDate, setMaxDate] = useState(31);
@@ -17,12 +15,6 @@ const MyPageEdit = () => {
   const thirty = ["4", "6", "9", "11"];
 
   const onChange = (e) => {
-    if (e.target.id === "name") {
-      setName(e.target.value);
-    }
-    if (e.target.id === "email") {
-      setEmail(e.target.value);
-    }
     if (e.target.id === "birth_month") {
       setBirthMonth(e.target.value);
       if (thirtyOne.includes(e.target.value)) {
@@ -40,11 +32,8 @@ const MyPageEdit = () => {
   return (
     <>
       <h1>마이 페이지</h1>
-      <label htmlFor="name">이름</label>
-      <input id="name" onChange={onChange} type="text" value={name} />
-
-      <label htmlFor="email">이메일</label>
-      <input id="email" onChange={onChange} type="text" value={email} />
+      <p>이름 {currName}</p>
+      <p>카카오계정 이메일 {currEmail}</p>
       <p>생일</p>
       <input
         id="birth_month"
