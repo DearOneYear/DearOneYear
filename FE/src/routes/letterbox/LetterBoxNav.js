67a65acb44f9d56d33b5b672274764d28bfe9204
Suji Cha <suji0509@naver.com>
2022-11-26 20:54:05 +0900

    design: 편지함 디자인 적용

--- FE/src/routes/letterbox/LetterBoxNav.js
+++ FE/src/routes/letterbox/LetterBoxNav.js
@@ -6,6 +6,41 @@ const Nav = styled.div`
   display: flex;
   justify-content: space-evenly;
 `;
+const Tap1 = styled.p`
+  position: absolute;
+  width: 6.75rem;
+  height: 1.5rem;
+  left: 1.5rem;
+  top: 9.125rem;
+
+  font-family: "MapoGoldenPier";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+
+  color: #ffffff;
+
+  text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
+`;
+
+const Tap2 = styled.p`
+  position: absolute;
+  width: 6.75rem;
+  height: 1.5rem;
+  left: 9.75rem;
+  top: 9.125rem;
+
+  font-family: "MapoGoldenPier";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+
+  color: #ffffff;
+
+  text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
+`;
 
 const LetterBoxNav = () => {
   return (
@@ -14,13 +49,13 @@ const LetterBoxNav = () => {
         to="/letterbox/unread"
         style={{ color: "black", textDecoration: "none" }}
       >
-        <span>안 읽은 편지</span>
+        <Tap1>기다리는 중</Tap1>
       </Link>
       <Link
         to="/letterbox/read"
         style={{ color: "black", textDecoration: "none" }}
       >
-        <span>읽은 편지</span>
+        <Tap2>읽은 편지함</Tap2>
       </Link>
     </Nav>
   );
