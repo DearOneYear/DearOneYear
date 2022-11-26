33fee158a7a2476a18131a0776fe6675ea04702c
fortepiano13 <severus007@naver.com>
2022-11-26 16:20:53 +0900

    로그인 체크

--- FE/src/routes/main/Main.js
+++ FE/src/routes/main/Main.js
@@ -5,14 +5,28 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import dummyLetter from "../letterbox/dummy/dummyLetter.json";
+import Axios from "axios"
+
 
 
 
 function Main() {
-    const REST_API_KEY = "489dc458a9a2b77d9583aa0e98ef0f52";
-    const REDIRECT_URI = "http://localhost:3000";
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+    const KAKAO_AUTH_URL = "http://localhost:8000/accounts/signin/kakao/";
     
+    // 쿠키 받기
+    let [accessToken, setAccessToken] = useState('');
+
+    let cookie = document.cookie.split(';');
+    let cookieArr = [];
+      cookie.map((e) => {
+        let c = e.split('=');
+        cookieArr.push(c);
+      });
+      console.log(cookieArr);
+      setAccessToken(cookieArr[2][1]);
+
+    console.log(accessToken);
+
 
 
     return (
@@ -26,15 +40,25 @@ function Main() {
             <div>
                 <p>내일의 당신에게 편지하세요</p>
                 <h1>나의 내일에게</h1>
-                <a href={KAKAO_AUTH_URL}>
-                <button>로그인</button>
-                </a>
+                <Link to="/main/login">
+                    <button>로그인</button>
+                    
+                </Link>
             </div>
             <div>
                 <p>유리병을 눌러 익명의 편지를 읽어보세요</p>
                 <img onClick=""></img>
             </div>
             <div>
+                
+                {/* {accessToken !== null || accessToken !== '' } */}
+                {/* {accessToken이 있다 
+                    ? <Link to="/mypage">
+                        <button>편지하러 가기</button>
+                        //편지쓰기가 없어서 일단은 마이페이지 링크
+                    </Link>
+                } */}
+
                 <Link to="/mypage">
                     <button>편지하러 가기</button>
                     //편지쓰기가 없어서 일단은 마이페이지 링크
