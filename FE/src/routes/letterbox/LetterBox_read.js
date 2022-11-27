dc878fa18472a0d9eda7024415fb7a79b20bae0d
Suji Cha <suji0509@naver.com>
2022-11-28 01:51:47 +0900

    편지함 디자인 적용

--- FE/src/routes/letterbox/LetterBox_read.js
+++ FE/src/routes/letterbox/LetterBox_read.js
@@ -1,64 +1,201 @@
-import dummyLetter from "./dummy/dummyLetter.json";
+import new_dummy from "./dummy/new_dummy.json";
 import { Link, useNavigate } from "react-router-dom";
 import LetterBoxNav from "./LetterBoxNav";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillPersonFill, BsLink45Deg } from "react-icons/bs";
+
+const url = "/img/background.png";
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  background-image: url(${url});
+  background-repeat: no-repeat;
+  background-origin: padding-box;
+  background-size: cover;
+`;
+const Title = styled.p`
+  position: relative;
+  width: 4.563rem;
+  height: 2rem;
+  left: 1.5rem;
+  top: 5.625rem;
+
+  font-family: "MapoGoldenPier";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 2rem;
+
+  letter-spacing: 0.02rem;
+
+  color: #ffffff;
+
+  text-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const Letter = styled.div`
+  position: relative;
+  width: 19.25rem;
+  height: 3.625rem;
+  left: 1.688rem;
+  top: 1rem;
+`;
+
+const LetterTitle = styled.p`
+  position: relative;
+  height: 1.813rem;
+  left: 4.2rem;
+  top: -2.625rem;
+
+  font-family: "MapoGoldenPier";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 1.813rem;
+  /* identical to box height */
+
+  color: #ffffff;
+
+  /* 기본 그림자 */
+
+  text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.25);
+`;
+
+const LetterPeriod = styled.p`
+  position: relative;
+  height: 1.813rem;
+  left: 4.2rem;
+  top: -6.5rem;
+  margin: 0;
+
+  font-family: "MapoGoldenPier";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.1rem;
+  line-height: 1.813rem;
+  /* identical to box height */
+
+  color: #ffffff;
+
+  /* 기본 그림자 */
+`;
+
+const NewLetterBtn = styled.button`
+  position: relative;
+  width: 23.875rem;
+  height: 3.563rem;
+  top: 31rem;
+
+  background: rgba(50, 50, 50, 0.7);
+  border: 0.075rem solid #ffffff;
+  box-shadow: 0px 0.25rem 1.5rem -0.063rem rgba(0, 0, 0, 0.2);
+  backdrop-filter: blur(0.625rem);
+
+  border-radius: 0.625rem;
+`;
+
+const NewLetterTxt = styled.p`
+  position: relative;
+  width: 12.188rem;
+  height: 1.5rem;
+  top: 34.8rem;
+  z-index: 3;
+
+  font-family: "MapoGoldenPier";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 1.5rem;
+
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  color: #ffffff;
+`;
+
+const ClosedBottle = styled.img`
+  position: relative;
+  width: 2.938rem;
+  height: 58px;
+  top: 2.5rem;
+`;
 
 const LetterBoxRead = () => {
-  let [dbLetter, setDbLetter] = useState([]);
-  let [accessToken, setAccessToken] = useState('');
-
-  const getCookie = () => {
-    let cookie = document.cookie.split(';');
-    let cookieArr = [];
-    cookie.map((e) => {
-      let c = e.split('=');
-      cookieArr.push(c);
-    });
-    setAccessToken(cookieArr[2][1]);
-  }
-
-
-  const getLetter = async () => { 
-    await axios.get('http://localhost:8000/letter/postbox/', { headers: { Authorization: `Bearer ${accessToken}`} }).then((res) => {
-    setDbLetter([...res.data]);
-  }).catch(function (err) {
-    console.log(err)
-  });
-  }  
-    
-
-  dbLetter.map((e) => {
-    let open = e.openAt.split('T')[0].split('-');
+  // 백 없이 작업 (더미)
+  let openedLetters = [new_dummy];
+  openedLetters.map((e) => {
+    let open = e.openAt.split("T")[0].split("-");
     e.openYear = open[0];
     e.openMonth = open[1];
     e.openDate = open[2];
-    
-    let send = e.sendAt.split('T')[0].split('-');
+
+    let send = e.sendAt.split("T")[0].split("-");
     e.sendYear = send[0];
     e.sendMonth = send[1];
     e.sendDate = send[2];
-  })
-
-  // console.log(dbLetter);
-
-  useEffect(() => {
-    getCookie();
-    getLetter();
-  }, []);
-
-
-
-
-
-  let openedLetters = [];
-  dbLetter.map((l) => {
-    if (l.isOpend === true) {
-      openedLetters.push(l);
-    }
   });
 
-  console.log(openedLetters);
+  // let [dbLetter, setDbLetter] = useState([]);
+  // let [accessToken, setAccessToken] = useState("");
+
+  // const getCookie = () => {
+  //   let cookie = document.cookie.split(";");
+  //   let cookieArr = [];
+  //   cookie.map((e) => {
+  //     let c = e.split("=");
+  //     cookieArr.push(c);
+  //   });
+  //   setAccessToken(cookieArr[2][1]);
+  // };
+
+  // const getLetter = async () => {
+  //   await axios
+  //     .get("http://localhost:8000/letter/postbox/", {
+  //       headers: { Authorization: `Bearer ${accessToken}` },
+  //     })
+  //     .then((res) => {
+  //       setDbLetter([...res.data]);
+  //     })
+  //     .catch(function (err) {
+  //       console.log(err);
+  //     });
+  // };
+
+  // dbLetter.map((e) => {
+  //   let open = e.openAt.split("T")[0].split("-");
+  //   e.openYear = open[0];
+  //   e.openMonth = open[1];
+  //   e.openDate = open[2];
+
+  //   let send = e.sendAt.split("T")[0].split("-");
+  //   e.sendYear = send[0];
+  //   e.sendMonth = send[1];
+  //   e.sendDate = send[2];
+  // });
+
+  // // console.log(dbLetter);
+
+  // useEffect(() => {
+  //   getCookie();
+  //   getLetter();
+  // }, []);
+
+  // let openedLetters = [];
+  // dbLetter.map((l) => {
+  //   if (l.isOpend === true) {
+  //     openedLetters.push(l);
+  //   }
+  // });
+
+  // console.log(openedLetters);
 
   //링크 공유하기
   let url = document.location.href;
@@ -80,52 +217,77 @@ const LetterBoxRead = () => {
   };
 
   return (
-    <>
-      <h1>편지함</h1>
-      <button onClick={goBack}>뒤로 가기</button>
-      <br />
-      <br />
+    <Container>
+      <Title>편지함</Title>
+      <AiFillHome
+        onClick={() => navigate("/")}
+        style={{
+          color: "white",
+          position: "relative",
+          width: "2.125rem",
+          height: "2.125rem",
+          left: "19.3rem",
+          top: "2rem",
+        }}
+      />
+      <BsFillPersonFill
+        onClick={() => navigate("/mypage")}
+        style={{
+          color: "white",
+          position: "relative",
+          width: "2.125rem",
+          height: "2.125rem",
+          left: "20rem",
+          top: "2rem",
+        }}
+      />
+
       <LetterBoxNav />
-      <br />
 
       {openedLetters.map((letter) => (
-        <div key={letter.id} id={letter.id}>
-          <Link
-            to={`/detail/${letter.id}`}
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            {letter.isOpend === true ? (
-              <img style={{ width: "10%" }} src="/img/open.png" alt="open" />
-            ) : (
-              <img style={{ width: "10%" }} src="/img/close.png" alt="close" />
-            )}
-            {/* <img style={{ width: "10%" }} src="/img/close.png" alt="close" /> */}
-          </Link>
-          <p>{letter.ddayInfo}</p>
-
-          {letter.recipient !== letter.sender ? (
-            <span>{letter.sender} 에게</span>
+        <Letter key={letter.id} id={letter.id}>
+          {letter.isOpend === true ? (
+            <img style={{ width: "10%" }} src="/img/open.png" alt="open" />
           ) : (
-            <span>나에게</span>
+            <ClosedBottle src="/img/letter.png" alt="close" id={letter.id} />
           )}
 
-          {letter.recipient !== letter.sender ? (
-            <span onClick={onShareClick} id={letter.id}>
-              🔗
-            </span>
+          {letter.to_name !== letter.from_name ? (
+            <LetterTitle id={letter.id}>
+              D - {letter.dday} {letter.to_name}에게
+            </LetterTitle>
+          ) : (
+            <LetterTitle id={letter.id}>나에게</LetterTitle>
+          )}
+
+          {letter.to_name !== letter.from_name ? (
+            <BsLink45Deg
+              style={{
+                color: "white",
+                position: "relative",
+                width: "2.125rem",
+                height: "2.125rem",
+                left: "20rem",
+                top: "-6rem",
+              }}
+              onClick={onShareClick}
+              id={letter.id}
+            />
           ) : (
             <></>
           )}
 
-          <p>
-            {`${letter.sendAt[0]}년 ${letter.sendAt[1]}월 ${letter.sendAt[2]}일`}
-            →
-            {`${letter.openAt[0]}년 ${letter.openAt[1]}월 ${letter.openAt[2]}일`}
-          </p>
-          <hr />
-        </div>
+          <LetterPeriod>
+            {`${letter.sendYear}.${letter.sendMonth}.${letter.sendDate}.`} →{" "}
+            {`${letter.openYear}.${letter.openMonth}.${letter.openDate}.`}
+          </LetterPeriod>
+        </Letter>
       ))}
-    </>
+      <center>
+        <NewLetterTxt>새로운 편지하러 가기</NewLetterTxt>
+        <NewLetterBtn></NewLetterBtn>
+      </center>
+    </Container>
   );
 };
 
