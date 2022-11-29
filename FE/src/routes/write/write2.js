cf11f8418d1d54fdd8f49dcc43ffdd22bb4f80c3
HyeyoungEum <djagpdud125@naver.com>
2022-11-29 22:53:15 +0900

    HY 221129 22:52 백과 연결 중

--- FE/src/routes/write/write2.js
+++ FE/src/routes/write/write2.js
@@ -7,31 +7,31 @@ const Title = styled.h2`
 
 function Write2() {
     const navigate = useNavigate();
-    let text1 = '';
-    let text2 = '';
+    let toname = '';
+    let toyou = '';
 
     function Navigate() {
-        navigate(`/write/write3/toyou`, { state : { whoName1 : text1, whoName2 : text2 }});
+        navigate(`/write/write3/toyou`, { state : { toname : {toname}, toyou : {toyou} }});
     };
 
-    function printText1() {
-        text1 = document.getElementById('toname').value;
-        document.getElementById("result1").innerText = text1
+    function printToName() {
+        toname = document.getElementById('toname').value;
+        document.getElementById("result1").innerText = toname
     };
 
-    function printText2() {
-        text2 = document.getElementById('toyou').value;
-        document.getElementById("result2").innerText = text2
+    function printToYou() {
+        toyou = document.getElementById('toyou').value;
+        document.getElementById("result2").innerText = toyou
     };
 
     return (
     <>
     <Title>너에게 편지쓰는 중...(1/5)</Title>
         <label htmlFor="toname">To. </label>
-        <input id="toname" type="text" placeholder="받는 사람 이름" maxLength="28" required onKeyUp={printText1}></input>님
+        <input id="toname" type="text" placeholder="받는 사람 이름" maxLength="28" required onKeyUp={printToName}></input>님
         <br></br>
         <label htmlFor="toyou">From. </label>
-        <input id="toyou" type="text" placeholder="내 이름" maxLength="28" required onKeyUp={printText2}></input>님
+        <input id="toyou" type="text" placeholder="내 이름" maxLength="28" required onKeyUp={printToYou}></input>님
         <p>나중에 편지 도착을 알려드려요! <br></br>ex "<span id="result1" ></span>"님에게 "<span id="result2"></span>"님이 보낸 편지가 도착했어요</p>
     <button onClick={Navigate}>다음으로</button>
 
