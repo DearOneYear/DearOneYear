6b7e00cc56427bd6d622b9ed89dcfac48350cd76
fortepiano13 <severus007@naver.com>
2022-11-30 20:32:33 +0900

    모달 디자인 적용

--- FE/src/routes/main/Modal.js
+++ FE/src/routes/main/Modal.js
@@ -23,8 +23,17 @@ export default function Modal() {
         <div className="modal">
           <div className="overlay"></div>
           <div className="modal-content">
-            <h2>Hello Modal</h2>
-            <p>내용입니다</p>
+          <h2 className="myunderline1">2023년 겨울의 유진에게</h2>
+          <p className="myunderline1">안녕, 유진아! 잘 지내? 부디 그땐 더 행복한
+            나였으면 좋겠다. 요즘 뭘 해도 재미가 없고 주
+            위의 멋진 친구들을 보며 초라해지는 것 같기
+            도 해. 그래도 난 언제나 그랬듯이 다시 회복하
+            고 앞으로 나아갈 거야. 그렇지? 지금 이 걱정
+            과 우울함이 지나고 보니 별거 아니었다고 말
+            할 수 있는 2023년의 유진이 되길 바라며, 조금
+            만 쉬고 다시 나아갈게! 언제나 그 누구보다, 
+            세상에서 제일 너를 응원하고 사랑해</p>
+          <h2 className="myunderline1">2022년 겨울의 유진이</h2>
             <button className="close-modal" onClick={toggleModal}>
               CLOSE
             </button>
