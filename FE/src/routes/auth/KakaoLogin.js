67b06257af705ad46dd5c8844655f4667d5a5d7b
kchaehyeon01 <kchaehyeon01@gmail.com>
2022-12-15 14:31:37 +0900

    design: KaKaoLogin.js css 추가

--- FE/src/routes/auth/KakaoLogin.js
+++ FE/src/routes/auth/KakaoLogin.js
@@ -55,22 +55,94 @@ const KakaoLogin = () => {
   }, []);
 
   return (
-    <>
-      <IoIosArrowBack onClick={() => navigate(-1)} />
-      <p>로그인 / 회원가입</p>
-      <>
-        <p>세상에서 가장 소중한 사람,</p>
-        <p>바로 당신에게 편지하세요.</p>
-        <p>세상에서 가장 큰 위로, 응원, 사랑이</p>
-        <p>되어드릴게요.</p>
-      </>
-      <img
+    <CenterWrapper>
+    <MainWrapper>
+      <DivTop>
+        <DivArrowButton>
+          <IoIosArrowBack onClick={() => navigate(-1)} />
+        </DivArrowButton>
+        <PLogin>로그인 / 회원가입</PLogin>
+      </DivTop>
+      <DivMid>
+        <PComment>세상에서 가장 소중한 사람,</PComment>
+        <PComment>바로 당신에게 편지하세요.</PComment>
+        <PComment>세상에서 가장 큰 위로, 응원, 사랑이</PComment>
+        <PComment>되어드릴게요.</PComment>
+      </DivMid>
+      <ImgBottle
+        src="/img/closedbottle.png"
+      />
+      <ImgLoginButton
         alt="카카오 로그인 버튼"
         src="/img/kakaologin.png"
         onClick={onClickLogin}
       />
-    </>
+    </MainWrapper>
+    </CenterWrapper>
   );
 };
 
 export default KakaoLogin;
+
+const CenterWrapper = styled.div`
+  width: 100vw;  
+  height: 100vh;
+  display: grid;
+  justify-content: center;
+  background-color: black;
+`;
+
+const MainWrapper = styled.div`
+  width: 53vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  background-image: url("img/beach.png");
+  background-size: cover; 
+  background-position: center;
+  color: white;
+`;
+
+const DivTop = styled.div`
+  width: 100%;
+  margin: 1vh 0vh;  
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const DivArrowButton = styled.div`
+  padding-left: 2.5vh;
+`;
+
+const PLogin = styled.p`
+  font-size: 2.5vh;
+  font-weight: bold;
+  margin: 3vh;
+  text-shadow: 0vh 0.2vh 0.3vh rgba(0, 0, 0, 0.2);
+`;
+
+const DivMid = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 12vh 0vh;
+`;
+
+const PComment = styled.p`
+  font-size: 2.5vh;
+  text-shadow: 0vh 0.2vh 0.3vh rgba(0, 0, 0, 0.2);
+`;
+
+const ImgBottle = styled.img`
+  width: 20vh;
+`;
+
+const ImgLoginButton = styled.img`
+  position: absolute;
+  width: 47vh;
+  height: 7.5vh;
+  border-radius: 1.02vh;
+  margin-top: 76.5vh;
+`;
